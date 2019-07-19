b4d4595a1e1add8b4b146d9dcd37a7b832016075
Chi Huynh <chihuynh@skyhawk-n277gt.gateway.sonic.net>
2019-07-19 13:20:43 -0700

    added functions for logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,26 @@
 
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[1];
 
-
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("The user flipped " + cardsInPlay[0]);
-console.log("The user flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("sorry try again");
-	}
+function checkForMatch(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			console.log("You found a match!");
+		} else {
+			console.log("sorry try again");
+		};
+	};
 }
+
+
+function flipCard(cardId){
+
+	console.log('user flipped '+ cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
+
+	
