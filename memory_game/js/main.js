54469e68f3de22c8afe86de74a1c66f7a6c4da84
Chi Huynh <chihuynh@skyhawk-n277gt.gateway.sonic.net>
2019-07-20 09:28:04 -0700

    changed arrays of cards to objects and added playing functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: 	"images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: 	"images/queen-of-diamonds.png"
+}
+];
 const cardsInPlay = [];
 
 function checkForMatch(){
@@ -15,12 +36,16 @@ function checkForMatch(){
 
 function flipCard(cardId){
 
-	console.log('user flipped '+ cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log('user flipped '+ cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 	checkForMatch();
 }
 
 flipCard(0);
 flipCard(1);
+
+
 
 	
